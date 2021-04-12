1fe5e047d1640eacfe7e44d07bf83b3021eca4de
Quinn T <Quinn.GitHub@gmail.com>
2021-04-11 22:01:42 -0500

    DOING THIS NOT HOMEWORK LULW

--- src/index.js
+++ src/index.js
@@ -1,9 +1,65 @@
-(()=>{
-    if (args.length == 0) {
-        throw new Error("Please provide some arguments, use 'help' for help.");
+(function(){
+    const invoc = '$$ dam';
+    if (args[0] === undefined) return `abb ac 0 a em:\u{22}Please provide a subcommand: try '${invoc} help.\u{22}`;
+
+
+    /** @type {[x: {name:string;aliases: Array<string>|null; code: () => string;help:string|null;}]} */
+    const cmds = {
+        install: {
+            name: 'install',
+            aliases: ['i'],
+            code: function () {
+                if(3 <= args.length && args.length <= 4) {
+                    return `pbg ${args.slice(1).join(' ')} \u{7c} abb say \u{7c} null \u{7c} tt trim $`+
+                        `{0+ } \u{7c} alias add ${args[2]} pbg ${args[1]} \u{7c} null \u{7c} abb say`+
+                        ` Success! Paste \u{22}${args[1]}\u{22} added as alias \u{22}${args[2]}\u{22}.`;
+                } else
+                    return `abb ac 0 a em:\u{22}Expected arguments: ${invoc} install (pastebin) (alias name) [force:true]\u{22}`;
+            },
+        },
+        help: {
+            name: 'help',
+            aliases: null,
+            code: function () {
+                let query = args.slice(1);
+                let cmd = cmds[query[0]];
+                if (!cmd)
+                    for (let cmdName in cmds) {
+                        if (cmdName === query[0]) {
+                            cmd = cmds[cmdName];
+                            break;
+                        }
+                    };
+                let help = cmd.help ?? '(No help found)';
+                return `abb say Help for ${cmd.name}: ${help}`
+            },
+            help: `Use '${invoc} help (subcommand)' for more information on that subcommand.`
+        }
+    };
+    let cmd = cmds[args[0]];
+    if (!cmd) {
+        for (let cmdName in cmds) {
+            if (cmds[cmdName].aliases?.includes(args[0])) {
+                cmd = cmds[cmdName];
+            }
+            if (!cmd) return `abb ac 0 a em:\u{22}Subcommand '${args[0]}' does not exist, try '${invoc} help'\u{22}`;
+            break;
+        }
     }
+
+
+    return cmd.code();
+/*
     switch (args[0]) {
-        case "help":
-            return 'abb say No help info LULW';
+        case 'i':
+        case 'install':
+
+        case 'help':
+
+        case undefined:
+            return `abb ac 0 a em:\u{22}Please provide a subcommand: try '${invoc} help.\u{22}`;
+        default:
+            return `abb ac 0 a em:\u{22}Unrecognised subcommand, try '${invoc} help'.\u{22}`;
     }
+*/
 })();
--- tests/index.js
+++ tests/index.js
@@ -1,47 +1,63 @@
-// const files = [
-//     "objects/date",
-//     "objects/error",
-//     "objects/errors",
-//     "objects/promise",
-//     "objects/url-params",
 
-//     "singletons/query",
-//     "classes/config",
-//     "singletons/utils",
-//     "classes/cron",
-//     "singletons/cache",
-//     "singletons/cooldown-manager",
-//     "singletons/logger",
-//     "singletons/system-log",
-//     "singletons/vlc-connector",
-//     "singletons/twitter",
-//     "singletons/internal-request",
-//     "singletons/local-request",
-//     "singletons/runtime",
-//     "singletons/sandbox",
+(async()=>{
+    await require("supi-core")("sb", {
+        whitelist: [
+            "objects/date",
+            "singletons/sandbox",
+            "singletons/utils"
+        ]
+    })
+    const fs = require("fs").promises;
+    const dankdebug = require("./dankdebug");
+    const Prompt = require("prompt-sync")
 
-//     "classes/got",
-//     "singletons/pastebin",
+    let prompt = Prompt({
+        sigint: true
+    });
 
-//     "classes/platform",
-//     "classes/filter",
-//     "classes/command",
-//     "classes/channel",
-//     "classes/chat-module",
-//     "classes/user",
-//     "classes/afk",
-//     "classes/banphrase",
-//     "classes/reminder"
-// ];
 
-require("supi-core")("sb", {
-    whitelist: [
-        "objects/date",
-        "singletons/sandbox",
-        "singletons/utils"
-    ]
-})
+    while(!0) {
 
-let d = new sb.Date();
+        let args = [];
+        let _args = prompt("$$ dam ")
+            .split(/ +/)
+            .map(
+                (value)=>{
+                    return value === '' ? undefined : value
+                })
+        for(const arg of _args) {
+            if(arg !== undefined){
+                args.push(arg)
+            }
+        };
+        let script = (await fs.readFile("./src/index.js"))
+            .toString()
+            // Remove multiline comments
+            .replace(/(\/\*)([\s\S]*?)?(\*\/)/g, "")
+            // Turn whitespace into 1 space
+            .replace(/\s+/g, " ")
+            // remove newlines.
+            .replace(/\r\n|\r|\n/g, "")
+        let pasteText = `dankdebug function:"${script}"`
+        await fs.writeFile("./out/script.js", script)
+        await fs.writeFile("./out/paste.txt", pasteText)
+        
+        
+        let result = await dankdebug.Code({ params: { function: script } }, ...args);
+        result.success = result.success ?? true
+        console.log(result)
 
-d.format("")
+        if( script.includes('"') ) console.error( new Error(`Script cannot contain double quotes (\").`))
+        if( script.includes('|') ) console.error( new Error(`Script cannot contain pipe ('|').`))
+        if( script.includes('>') ) console.error( new Error(`Script cannot contain greater than ('>').`))
+        if( pasteText.length > 50_000 ) { console.error( new Error( `Script length is above 50 000 (${ script.length }).` ))} else
+        if( pasteText.length > 40_000 ) { console.warn( new Error( `Script length aproaching 40 000 (${ script.length }).` ))} else
+        console.log(`Length of script: ${pasteText.length}`);
+    }
+})();
+
+
+
+
+
+
