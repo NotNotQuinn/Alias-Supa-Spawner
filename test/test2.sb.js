1a284f40dc23c340e063af765d3fc19fa53df885
Quinn T <Quinn.GitHub@gmail.com>
2021-05-22 00:54:41 -0500

    tests: add test2

---
+++ test/test2.sb.js
@@ -0,0 +1,6 @@
+if (!args) return `This code should be executed using the function:"code" syntax.`;
+
+return utils.getEmote;
+// let fallback = args.pop();
+
+// return utils.getEmote(args, fallback);
